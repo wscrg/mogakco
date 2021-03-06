64e1d28e1063c9a5086939bf4b72e4165e6c931c
Ji-Yong, Jeong <57584529+wscrg@users.noreply.github.com>
2021-03-06 20:58:54 +0900

    프론트 Next, React 초기 세팅 (#5)

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dummy from '../components/Dummy';
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Index page</h1>
+      <Dummy />
+    </div>
+  );
+};
+
+export default Index;
