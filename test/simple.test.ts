843cb76ec1d56f3d726ce408ce10bb71f4c5b707
최은기 <galaxyhi4276@gmail.com>
2021-03-07 22:34:12 +0900

    기타: git action을 정상작동을 위한 테스트 커밋

--- test/simple.test.ts
+++ test/simple.test.ts
@@ -1,9 +1,8 @@
-import { AppController } from "../dist/app.controller";
-import { AppService } from "../services/app.service";
+// eslint-disable-next-line import/extensions,import/no-unresolved
+import { returnTwo } from './testModule';
 
-describe('컨트롤러와 서비스 간단 테스트', function () {
-
+describe('컨트롤러와 서비스 간단 테스트', () => {
   it('2 + 2는 4가 된다.', () => {
-    expect(2 + 2).toBe(4);
+    expect(returnTwo()).toBe(4);
   });
 });
